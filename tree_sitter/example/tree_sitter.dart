ccf4a033d2785afd931210f9e55b87763c40691a
Tim Whiting <tim@whitings.org>
2023-10-17 14:43:09 -0600

    update pubspec constraints, and add sample from moduloValue

--- tree_sitter/example/tree_sitter.dart
+++ tree_sitter/example/tree_sitter.dart
@@ -7,7 +7,15 @@ void main() {
   final parser = Parser(
       sharedLibrary: resolveDylibPath('dart', path: Directory.current.path),
       entryPoint: 'tree_sitter_dart');
-  final program = "class A {}";
+  const program = "class A {}";
+//   const program = r"""
+// void main() {
+//   test('', () {
+//     'åÅ';
+//     'åÅ';
+//   });
+// }
+// """;
   final tree = parser.parse(program);
   print(tree.root.child(0).namedChild(0).string);
   print(parser.getText(tree.root.child(0).namedChild(0)));
